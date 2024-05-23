11b57907ca02880ab918bcc6df2a9a5883e4fbb9
g1ann0z <giannozcydia@gmail.com>
2024-05-23 11:19:08 +0200

    completeTabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   {
     path: 'elements',
-    loadChildren: () => import('./elements/elements.module').then(m => m.ElementsModule) //lazy loading
+    loadChildren: () => import('./elements/elements.module')
+    .then(m => m.ElementsModule) //lazy loading
   },
   {
     path: 'collections',
-    loadChildren: () => import('./collections/collections.module').then(m => m.CollectionsModule) //lazy loading
+    loadChildren: () => import('./collections/collections.module')
+    .then(m => m.CollectionsModule) //lazy loading
   },
   {
     path: '',
