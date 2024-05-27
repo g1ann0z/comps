ccbc9a26cfe7ca6b2f02b77691b94813dd96a9c6
g1ann0z <giannozcydia@gmail.com>
2024-05-27 13:07:06 +0200

    component-done

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -8,6 +8,18 @@ import { Component } from '@angular/core';
 export class ModsHomeComponent {
 modalOpen = false;
 
+  items = [
+    { title: "Quali sono i benefici di una dieta equilibrata?",
+      content: "Migliora la salute cardiovascolare, aiuta a mantenere un peso sano, aumenta l'energia, rafforza il sistema immunitario e migliora la digestione."
+    },
+    { title: "Che cosa è l'energia rinnovabile?",
+    content: "È l'energia proveniente da fonti naturali che si rigenerano rapidamente, come il sole, il vento, l'acqua, il calore terrestre e la biomassa."
+    },
+    { title: "Quali sono i vantaggi dell'apprendimento online?",
+    content: "Offre flessibilità, accessibilità globale, una varietà di risorse, costi ridotti e percorsi di apprendimento personalizzati."
+    }
+  ];
+
   onClick(){
     this.modalOpen = !this.modalOpen;
   }
